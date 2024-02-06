db11fa06ea1be90783a839cce815fc28bde489f8
mike-subtilis <mike@subtilis-software.com>
2024-02-06 08:23:02 -0700

    authorization middleware

---
+++ src/api/authorization-middleware.js
@@ -0,0 +1,106 @@
+const { asyncEvery, asyncSome, intersection } = require('../util/arrayUtil');
+const hardcodedAuthorization = require('./hardcoded-authorization.json');
+
+module.exports.create = ({ entityRepo }) => {
+  function doValuesMatch(a, b) {
+    if (Array.isArray(a)) {
+      if (Array.isArray(b)) {
+        return intersection(a, b).length > 0;
+      }
+      return a.includes(b);
+    }
+    if (Array.isArray(b)) {
+      return b.includes(a);
+    }
+    return a === b;
+  }
+
+  async function passesGrantPolicyMatch(m, req) {
+    // m: { user, target, fixed } <-- must have exactly 2 of those fields
+    if (!Object.keys(m).length === 2) {
+      throw new Error(`Grant policy match should have exactly 2 fields (found ${Object.keys(m).join(',')})`);
+    }
+    const operands = [];
+    if (m.user) {
+      operands.push(req.user[m.user]);
+    }
+    if (m.fixed) {
+      operands.push(m.fixed);
+    }
+    if (m.target) {
+      if (req._cache && req._cache.target) {
+        operands.push(req._cache.target[m.target]);
+      }
+      const mockedTarget = { ...req.params, ...req.query };
+      if (mockedTarget[m.target]) {
+        operands.push(mockedTarget[m.target]);
+      } else {
+        const target = await entityRepo.get(req.params.id);
+        if (!target) {
+          return false;
+        }
+        req._cache = req._cache || {};
+        req._cache.target = target;
+        operands.push(target[m.target]);
+      }
+    }
+    return doValuesMatch(operands[0], operands[1]);
+  }
+
+  async function passesGrantPolicy(p, req) {
+    // p: { name, matches: true || [{ user, target, fixed }], fields: [] }
+    if (p.fields) {
+      if (Object.keys(req.body || {}).some(k => !p.fields.includes(k))) {
+        return false;
+      }
+    }
+    if (typeof p.matches === 'boolean' && !p.matches) {
+      return false;
+    }
+    if (Array.isArray(p.matches)) {
+      const matchesEvery = await asyncEvery(p.matches, async m => passesGrantPolicyMatch(m, req));
+      if (!matchesEvery) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  async function passesGrantPolicies(grantKey, req) {
+    if (Object.hasOwn(hardcodedAuthorization, grantKey) && hardcodedAuthorization[grantKey]) {
+      const grantPolicies = hardcodedAuthorization[grantKey];
+      if (typeof grantPolicies === 'boolean') {
+        return true;
+      }
+      if (Array.isArray(grantPolicies)) {
+        const somePoliciesPass = await asyncSome(grantPolicies, async p => passesGrantPolicy(p, req));
+        if (somePoliciesPass) {
+          return true;
+        }
+      } else {
+        throw new Error(`Grant policies for ${grantKey} are not well defined`);
+      }
+    }
+    return false;
+  }
+
+  return grantKey => ((req, res, next) => {
+    try {
+      const tokens = grantKey.split(':');
+      const entityType = tokens[0];
+      const actionType = tokens[1];
+      // const subActionType = tokens.length > 2 ? tokens[2] : null;
+
+      const parentGrantKey = `${entityType}:${actionType}`;
+      if (passesGrantPolicies(grantKey, req)) {
+        next();
+      } else if (parentGrantKey !== grantKey && passesGrantPolicies(parentGrantKey, req)) {
+        next();
+      }
+    } catch (ex) {
+      next(ex);
+    }
+    next(new Error(`You do not have '${grantKey}' access to this endpoint`));
+  });
+};
--- src/api/base-express-entity-api.js
+++ src/api/base-express-entity-api.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const authorizationFactory = require('./authorization-middleware');
 
 module.exports.create = (repo, entityType, options = {}) => {
   const router = express.Router();
   const entityRepo = repo[entityType];
-
-  router.get('/', (req, res) => {
+  const authorizer = authorizationFactory.create({ repo });
+  router.get('/', authorizer(`${entityType}:list`), (req, res) => {
     const { page = 1, pageSize = 25, ...rest } = req.query;
     entityRepo.getPage(page, pageSize, rest)
       .then((results) => {
@@ -12,7 +13,7 @@ module.exports.create = (repo, entityType, options = {}) => {
       });
   });
 
-  router.get('/:id', (req, res) => {
+  router.get('/:id', authorizer(`${entityType}:read`), (req, res) => {
     entityRepo.get(req.params.id, req.query[entityRepo.partitionField])
       .then((results) => {
         res.json(results);
@@ -20,21 +21,21 @@ module.exports.create = (repo, entityType, options = {}) => {
   });
 
   if (!options.readOnly) {
-    router.post('/', (req, res) => {
+    router.post('/', authorizer(`${entityType}:create`), (req, res) => {
       entityRepo.create(req.body, req.user, req.query[entityRepo.partitionField])
         .then((results) => {
           res.json(results);
         });
     });
 
-    router.put('/:id', (req, res) => {
+    router.put('/:id', authorizer(`${entityType}:update`), (req, res) => {
       entityRepo.update(req.params.id, req.query.etag, req.body, req.user, req.query[entityRepo.partitionField])
         .then((results) => {
           res.json(results);
         });
     });
 
-    router.delete('/:id', (req, res) => {
+    router.delete('/:id', authorizer(`${entityType}:delete`), (req, res) => {
       entityRepo.delete(req.params.id, req.query.etag, req.user, req.query[entityRepo.partitionField])
         .then(() => res.sendStatus(200));
     });
